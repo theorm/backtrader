84abdbca472d7f68080c7a8e3d5f4c2199c773d7
Roman Kalyakin <theorm@gmail.com>
2019-01-17 10:00:15 +0100

    Update __init__.py

--- backtrader/plot/__init__.py
+++ backtrader/plot/__init__.py
@@ -27,7 +27,7 @@ except ImportError:
     raise ImportError(
         'Matplotlib seems to be missing. Needed for plotting support')
 else:
-    matplotlib.use('TkAgg')
+    matplotlib.use('TkAgg', force=False)
 
 
 from .plot import Plot, Plot_OldSync
